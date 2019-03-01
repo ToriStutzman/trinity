52cb98583019745bb4ca74874833f421805a4e72
ToriStutzman <sstuutzzmaan@gmail.com>
2019-03-01 10:25:54 -0800

    Events header image class updated

--- events.php
+++ events.php
@@ -1,38 +1,10 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Events | Trinity Lutheran Church</title>
-	<link href="css/bootstrap-4.2.1.css" rel="stylesheet" type="text/css">	
-	<link href="css/style.css" rel="stylesheet" type="text/css">
-	<link href="css/calendar.css" rel="stylesheet" type="text/css">
-</head>
-<!-- WEBSITE WILL BREAK IF YOU DELETE/CHANGE STUFF ABOVE THIS POINT -->
-	
-	
-<body> <!-- WEBSITE BODY START -->
-	
-	<!-- HEADER START -->
-	<header>
-		
-		<!-- NAVIGATION START -->
-		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark"> <a class="navbar-brand" href="index.html">Trinity Lutheran Church</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-		    <ul class="navbar-nav ml-auto">
-				<li class="nav-item p-1"> <a class="nav-link" href="index.html">Home</a> </li>
-				<li class="nav-item p-1"> <a class="nav-link" href="about.html">About</a> </li>
-				<li class="nav-item p-1"> <a class="nav-link" href="events.html">Events</a> </li>
-			  	<li class="nav-item p-1"> <a class="nav-link" href="staff.html">Staff</a> </li>
-	        </ul>
-	    </div>
-	  </nav>		
-		<!-- NAVIGATION END -->
+<?php
+
+include('includes/head.php');
+?>
 		
 		<!-- Header Image -->
-		<img src="images/service.jpg" width="100%"/>
+		<img src="images/service.jpg" class="head-img"/>
     </header>
 	<!-- HEADER END-->
 	
@@ -46,15 +18,7 @@
 	</main> 
 	<!-- MAIN CONTENT END -->
 	
-	<!-- FOOTER START -->
-	<footer>
-	  <p><b>Trinity Lutheran Church</b> | 15534&nbsp;E.&nbsp;Marquam&nbsp;Rd.&nbsp;Mt.&nbsp;Angel,&nbsp;Oregon&nbsp;97362 <br> 503-333-3333  |  example@example.org</p>
-	</footer>	
-	<!-- FOOTER END -->
-	
-<!-- WEBSITE WILL BREAK IF YOU DELETE/CHANGE STUFF BELOW THIS POINT -->		
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap-4.2.1.js"></script>	
-</body> <!-- WEBSITE BODY END -->
-</html>
+<?php
+
+include('includes/footer.php');
+?>
