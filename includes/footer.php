e560497f8ec487115f594c52c6e7cadf6f444391
ToriStutzman <sstuutzzmaan@gmail.com>
2019-03-01 10:00:27 -0800

    Update footer and header php documents

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,13 @@
 	
+	<!-- FOOTER START -->
 	<footer>
 	  <p><b>Trinity Lutheran Church</b> | 15534&nbsp;E.&nbsp;Marquam&nbsp;Rd.&nbsp;Mt.&nbsp;Angel,&nbsp;Oregon&nbsp;97362 <br> 503-333-3333  |  example@example.org</p>
 	</footer>
-	
+	<!-- FOOTER END -->
 
-	
+<!-- YOU WILL BREAK STUFF IF YOU DELETE BELOW THIS POINT -->	
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap-4.2.1.js"></script>
-</body>
+</body> <!-- WEBSITE BODY END -->
 </html>
